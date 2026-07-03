7ed3f93d261ede619ca7926d9ab57e4d4e58af9d
formic-studio <kamil.k.kolodziejczyk@email.com>
2026-07-03 22:11:13 +0200

    btn hover v3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import '../styles/main.css';
+import { initButtonHover } from './button-hover.js';
 import { initCategoryStack } from './category-stack.js';
 import { initFitText } from './fit-text.js';
 import { initGigaMenu } from './giga-menu.js';
@@ -14,6 +15,7 @@ const initTheme = () => {
   initHeaderTheme();
   initHeaderScroll();
   initGigaMenu();
+  initButtonHover();
   void initIntroAnimation();
   initFitText();
   initCategoryStack();
